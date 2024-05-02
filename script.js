d88fde7a5eb1ccba0b20787da06a9fd0647fcd76
MelMino <160610954+MelMino@users.noreply.github.com>
2024-05-01 21:59:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function playVideo() {
+    var video = document.getElementById("my-video");
+    video.play();
+}
+
+function pauseVideo() {
+    var video = document.getElementById("my-video");
+    video.pause();
+}
